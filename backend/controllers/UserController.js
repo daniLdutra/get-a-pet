c4c26eba6ccd678ee69c852187752a1c3ca4b392
danildutra <danivetdutra@gmail.com>
2022-05-13 14:21:53 -0300

    Registrado usuario no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -40,6 +41,24 @@ module.exports = class UserController {
         .status(422)
         .json({ message: 'Email já cadastrado, utilize outro email' });
       return;
+    }
+
+    //create a password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //create a user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+    try {
+      const newUser = await user.save();
+      res.status(201).json({ message: 'Usuário criado', newUser });
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
   }
 };
